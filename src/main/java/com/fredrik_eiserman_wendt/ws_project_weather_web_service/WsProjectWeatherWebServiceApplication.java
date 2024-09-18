77cba88c5fb28d75fb948cf2bc234114a28221f8
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2024-09-18 20:38:54 +0200

    initial commit

---
+++ src/main/java/com/fredrik_eiserman_wendt/ws_project_weather_web_service/WsProjectWeatherWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fredrik_eiserman_wendt.ws_project_weather_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsProjectWeatherWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsProjectWeatherWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fredrik_eiserman_wendt/ws_project_weather_web_service/WsProjectWeatherWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredrik_eiserman_wendt.ws_project_weather_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsProjectWeatherWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
